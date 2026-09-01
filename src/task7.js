e33a8c71a0bf0dddd5566081478bfbe8714822e2
Daniel Castro <castroperezdaniel28@gmail.com>
2026-09-01 12:48:14 -0600

    Se finaliza los ejercicios del 1 al 6

--- src/task7.js
+++ src/task7.js
@@ -1,11 +1,20 @@
 const arr = [
-    [0,1,2,3,4,5,6,7,8,9],
-    [10,11,12,13,14,15,16,17,18,19],
-    [20,21,22,23,24,25,26,27,28,29]
-  ]
-  
-  // Type your code below this line!
-  
-  
-  
-  // Type your code above this line!
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+];
+
+// Type your code below this line!
+// ¿Pueden agregar un solo número a una fila existente?
+arr[0].push(100)
+console.log(arr);
+
+// ¿Pueden agregar una fila completa de números?
+let newNumber =[30,31,32,33,34,35,36,37,38,39]
+arr[3]=newNumber
+console.log(arr);
+
+// ¿Pueden eliminar un solo número de una fila específica?
+// ¿Pueden invertir una de las filas sin afectar a las demás?
+
+// Type your code above this line!
